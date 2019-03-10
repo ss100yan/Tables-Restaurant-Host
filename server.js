d7a707f6ddf4d4f78b9117f608aed26f23f65d3a
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-03-09 23:48:29 -0500

    new

---
+++ server.js
@@ -0,0 +1,87 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Reservations (DATA)
+// =============================================================
+var tables = [];
+
+var waitlist =[];
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+/* API ROUTES
+***************/
+//api/tables
+
+app.get("/api/tables", function(req, res) {
+    
+     return res.json(tables);
+  
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    
+    return res.json(waitlist);
+ 
+ });
+  
+
+
+
+app.post("/api/tables", function(req, res) {
+
+ // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+    var newReserve = req.body;
+
+    console.log(newReserve);
+
+    if(tables.length == 5){
+      waitlist.push(newReserve);
+    }else{
+      tables.push(newReserve);
+    }
+  
+    res.status(201).json(newReserve);
+  
+    // res.sendFile(path.join(__dirname, "view.html"));
+    
+  });
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//   newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
